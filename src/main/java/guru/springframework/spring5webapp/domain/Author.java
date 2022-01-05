3f80adb3baf7f83d7953965c4f9f16753a7d2281
igors <spitkovskyigor@gmail.com>
2022-01-05 19:44:24 +0200

    JPA entities added, repositories for these entities added, sprint-component bootstrap class added (assignment 2 completed)

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -16,10 +16,9 @@ public class Author {
     public Author() {
     }
 
-    public Author(String firstName, String lastName/*, Set<Book> books*/) {
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.books = books;
     }
 
     public String getFirstName() {
